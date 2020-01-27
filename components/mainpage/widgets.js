ac7479b0fc4dc84e25d87c47c907f8f15173dfda
areenk <areen.kim@vanderbilt.edu>
2020-01-26 22:46:20 -0500

    basic buttons and progress bar

--- components/mainpage/widgets.js
+++ components/mainpage/widgets.js
@@ -1,9 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, CheckBox } from 'react-native';
+        
+{/* do "$ npm install react-native-progress --save" to get progress bar */}
+import * as Progress from 'react-native-progress';
+ 
 
 export const WidgetContainer = () => {
     return (
-        <Text>Insert widgets here</Text>
+        <Div>
+
+            <Text>Water Intake</Text>
+        <Button title="Solid Button"/>
+
+        <Progress.Bar progress={0.3} width={200} />
+        <View style={styles.container}>
+        <Text style={styles.welcome}>Progress Example</Text>
+        <Progress.Bar
+          style={styles.progress}
+          progress={this.state.progress}
+          indeterminate={this.state.indeterminate}
+        />
+      </View>
+
+        <CheckBox
+            center
+            title='Food Intake'
+            checkedIcon='dot-circle-o'
+            uncheckedIcon='circle-o'
+            checked={this.state.checked}
+        />
+
+        <Text>Exercise</Text>
+        <Button title="Solid Button"/>
+
+        <Progress.Bar progress={0.3} width={200} />
+        <View style={styles.container}>
+        <Text style={styles.welcome}>Progress Example</Text>
+        <Progress.Bar
+          style={styles.progress}
+          progress={this.state.progress}
+          indeterminate={this.state.indeterminate}
+        />
+      </View>
+        </Div>
     );
 }
 
