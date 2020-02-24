58b7b553ba4ea0c3ae0ff61d02c99cffb8703108
areenk <areen.kim@vanderbilt.edu>
2020-02-24 12:52:00 -0600

    graphs

--- components/mainpage/widgets.js
+++ components/mainpage/widgets.js
@@ -1,4 +1,12 @@
 import React, { Component } from 'react';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from 'react-native-chart-kit'
 import { StyleSheet, Text, View, Button, CheckBox, Alert, Animated, TouchableWithoutFeedback, AppRegistry } from 'react-native';
 
 {/* do "$ npm install react-native-progress --save" to get progress bar */ }
@@ -76,6 +84,68 @@ const Widget = (props) => {
       </View>
   );
 }
+
+
+//Daily Chart shows X:time Y:mood-value
+//weekly chart shows Y:average-mood-value X:date-in-week
+//Is there a way to show X:date, Y:time, color of input dot [0~10]????
+const line = {
+  labels: ['2/23', '2/24', '2/25', '2/26', '2/27', '2/28', '2/29'],
+  datasets: [
+    {
+      data: [7, 2, 5, 4, 5, 8, 6],
+      strokeWidth: 2, // optional
+    },
+  ],
+};
+
+//make another line graph for recorded sleep hours
+const line = {
+  labels: ['2/23', '2/24', '2/25', '2/26', '2/27', '2/28', '2/29'],
+  datasets: [
+    {
+      data: [7, 6, 5, 8, 4, 5, 9],
+      strokeWidth: 2, // optional
+    },
+  ],
+};
+
+// Pie chart will be used to show balance between positive, negative, and neutral moods
+const pieData = [
+  {
+    name: 'Positive',
+    population: 40, //derive from number of positive inputs 7-10. no need to find percentages- the piechart will do it for you
+    color: 'rgba(131, 167, 234, 1)',
+    legendFontColor: '#7F7F7F',
+    legendFontSize: 15,
+  },
+  {
+    name: 'Negative',
+    population: 35,
+    color: '#F00',
+    legendFontColor: '#7F7F7F',
+    legendFontSize: 15,
+  },
+  {
+    name: 'Netural',
+    population: 25,
+    color: 'rgb(0, 0, 255)',
+    legendFontColor: '#7F7F7F',
+    legendFontSize: 15,
+  },
+];
+
+<PieChart
+      data={pieData}
+      width={screenWidth}
+      height={220}
+      chartConfig={chartConfig}
+      accessor="population"
+      backgroundColor="transparent"
+      paddingLeft="15"
+      absolute
+    />
+
 
 const styles = StyleSheet.create({
   viewContainer: {
